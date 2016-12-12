167918c8982172bb8add10e2ca48073f63a8fc20
nyamadori <nyamadorig@gmail.com>
2016-12-12 18:04:18 +0900

    Setup typescript + power-assert + webpack

--- karma.conf.js
+++ karma.conf.js
@@ -6,59 +6,45 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['mocha'],
     files: [
-      'test/**/*.js',
-      'test/*.js'
+      'test/**/*.ts',
+      'test/*.ts'
     ],
     exclude: [],
 
     preprocessors: {
-      'test/*_test.js': ['webpack'],
-      'test/**/*_test.js': ['webpack']
+      'test/*_test.ts': ['webpack'],
+      'test/**/*_test.ts': ['webpack']
     },
 
     reporters: ['progress'],
     browsers: ['Chrome', 'Firefox'],
+
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    singleRun: false,
+    concurrency: Infinity,
 
     webpack: {
       devtool: 'inline-source-map',
       module: {
         loaders: [
           {
-            test: /\.js$/,
+            test: /\.ts$/,
             exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-              presets: ['es2015'],
-              plugins: ['babel-plugin-espower']
-            }
+            loader: 'ts'
           },
           {
             test: /\.json$/,
             loader: 'json'
           }
+        ],
+
+        postLoaders: [
+          { test: /_test\.ts$/, loader: 'webpack-espower-loader' }
         ]
       }
-    },
-
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
+    }
   })
 }
