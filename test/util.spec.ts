be3ad14a2cc3d096a8dfc15d0c26f6ad6f36cbe4
nyamadori <nyamadorig@gmail.com>
2016-12-13 05:27:13 +0900

    Refactor test

--- test/util.spec.ts
+++ test/util.spec.ts
@@ -10,75 +10,65 @@ describe('util', function () {
     fixture.cleanup()
   })
 
-  describe('.getNth', function () {
+  describe('selectors', function () {
     beforeEach(function () {
-      fixture.load('getNth.html', true)
+      fixture.load('selectors.html', true)
     })
 
-    it('returns index of given element from the parent', function () {
-      const first = document.getElementById('first')
-      const second = document.getElementById('second')
-      const third = document.getElementById('third')
+    describe('.getNth', function () {
+      it('returns index of given element from the parent', function () {
+        const first = document.getElementById('first')
+        const second = document.getElementById('second')
+        const third = document.getElementById('third')
 
-      assert(util.getNth(first) === 0)
-      assert(util.getNth(second) === 1)
-      assert(util.getNth(third) === 2)
+        assert(util.getNth(first) === 0)
+        assert(util.getNth(second) === 1)
+        assert(util.getNth(third) === 2)
+      })
+
+      it('returns 0 when given element is root element', function () {
+        const ele = document.getElementsByTagName('html')[0]
+
+        assert(util.getNth(ele) === 0)
+      })
     })
 
-    it('returns 0 when given element is root element', function () {
-      const ele = document.getElementsByTagName('html')[0]
+    describe('.getNthSelector', function () {
+      it('returns nth-of-type selector with index of given element', function () {
+        const ele = document.getElementById('second')
 
-      assert(util.getNth(ele) === 0)
-    })
-  })
-
-  describe('.getNthSelector', function () {
-    beforeEach(function () {
-      fixture.load('getNth.html', true)
+        assert(util.getNthSelector(ele) === ':nth-of-type(1)')
+      })
     })
 
-    it('returns nth-of-type selector with index of given element', function () {
-      const ele = document.getElementById('second')
+    describe('.getAttrSelector', function () {
+      it('returns attribute selector', function () {
+        const ele = document.getElementById('dashboard-link')
 
-      assert(util.getNthSelector(ele) === ':nth-of-type(1)')
-    })
-  })
+        assert(util.getAttrSelector(ele, 'id', 'href') === '[id="dashboard-link" href="/dashboard"]')
+      })
 
-  describe('.getAttrSelector', function () {
-    beforeEach(function () {
-      fixture.load('getAttrSelector.html', true)
-    })
+      it('filters non given attributes to the element', function () {
+        const ele = document.getElementById('dashboard-link')
 
-    it('returns attribute selector', function () {
-      const ele = document.getElementById('dashboard-link')
+        assert(util.getAttrSelector(ele, 'id', 'css') === '[id="dashboard-link"]')
+      })
 
-      assert(util.getAttrSelector(ele, 'id', 'href') === '[id="dashboard-link" href="/dashboard"]')
-    })
+      it("returns an empty string when all given attributes wasn't specify on the element", function () {
+        const ele = document.getElementById('dashboard-link')
 
-    it('filters non given attributes to the element', function () {
-      const ele = document.getElementById('dashboard-link')
-
-      assert(util.getAttrSelector(ele, 'id', 'css') === '[id="dashboard-link"]')
+        assert(util.getAttrSelector(ele, 'class', 'css') === '')
+      })
     })
 
-    it("returns an empty string when all given attributes wasn't specify on the element", function () {
-      const ele = document.getElementById('dashboard-link')
+    describe('.getElementSelector', function () {
+      it('returns element selector', function () {
+        const div = document.getElementById('parent')
+        const p = document.getElementById('first')
 
-      assert(util.getAttrSelector(ele, 'class', 'css') === '')
-    })
-  })
-
-  describe('.getElementSelector', function () {
-    beforeEach(function () {
-      fixture.load('getElementSelector.html', true)
-    })
-
-    it('returns element selector', function () {
-      const anchorElement = document.getElementById('a')
-      const divElement = document.getElementById('div')
-
-      assert(util.getElementSelector(anchorElement) === 'a')
-      assert(util.getElementSelector(divElement) === 'div')
+        assert(util.getElementSelector(div) === 'div')
+        assert(util.getElementSelector(p) === 'p')
+      })
     })
   })
 })
