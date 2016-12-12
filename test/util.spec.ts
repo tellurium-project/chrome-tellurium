8ae2c3382a355915897d6046ea53e98bfed310a9
nyamadori <nyamadorig@gmail.com>
2016-12-13 04:55:27 +0900

    Add .getNthSelector spec

--- test/util.spec.ts
+++ test/util.spec.ts
@@ -1,18 +1,18 @@
 import * as assert from 'power-assert'
-import { getNth } from '../src/lib/util'
+import { getNth, getNthSelector } from '../src/lib/util'
 
 describe('util', function () {
   before(function () {
     fixture.setBase('test/fixtures/util')
   })
 
+  afterEach(function () {
+    fixture.cleanup()
+  })
+
   describe('.getNth', function () {
     beforeEach(function () {
       fixture.load('getNth.html', true)
-    })
-
-    afterEach(function () {
-      fixture.cleanup()
     })
 
     it('returns index of given element from the parent', function () {
@@ -25,10 +25,22 @@ describe('util', function () {
       assert(getNth(third) === 2)
     })
 
-    it('returns 0 when the element has no parent', function () {
+    it('returns 0 when given element is root element', function () {
       const ele = document.getElementsByTagName('html')[0]
 
       assert(getNth(ele) === 0)
+    })
+  })
+
+  describe('.getNthSelector', function () {
+    beforeEach(function () {
+      fixture.load('getNth.html', true)
+    })
+
+    it('returns nth-of-type selector with index of given element', function () {
+      const ele = document.getElementById('second')
+
+      assert(getNthSelector(ele) === ':nth-of-type(1)')
     })
   })
 })
