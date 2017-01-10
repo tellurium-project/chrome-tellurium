dd86e6cd1417d6cb4ae0a8cacf30966afe6c8e27
nyamadori <nyamadorig@gmail.com>
2017-01-10 18:35:08 +0900

    Use WebSocket for communication with the server

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -1,17 +1,19 @@
+import * as socketIO from 'socket.io-client'
+
+const io = socketIO('http://localhost:9000')
+io.on('connect', () => {
+  console.log(`Socket ID: ${io.id}`)
+})
+
+io.on('startCapture', (data) => {
+  console.log(`startCapturing: ${data}`)
+})
 
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
   switch (req.type) {
     case 'documentEvent':
       console.log(req.payload)
-      const request = new XMLHttpRequest()
-      request.addEventListener('readystatechange', () => {
-        if (request.readyState === 4 && request.status === 200) {
-          console.log('success')
-        }
-      })
-      request.open('POST', 'http://localhost:9000/sessions/x/events')
-      request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
-      request.send(JSON.stringify(req.payload))
+      io.emit('createOperation', req.payload)
       break
   }
 
